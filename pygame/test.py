024de09e62c69c99046700ab592a58275c876052
David O'Donohue <subjectification@gmail.com>
2019-02-01 14:09:53 +1100

    automatically committing

--- pygame/test.py
+++ pygame/test.py
@@ -11,24 +11,25 @@ from pygame.locals import *
 
 WIDTH = 800
 HEIGHT = 600
-GROUND = 400
+SKY = 400
 JUMP_APEX = 50
-CLOCK_TICK = 180
-ENEMY_INTERVAL = 5
+CLOCK_TICK = 500
+ENEMY_INTERVAL = 40
+ENEMY_WIDTH = 20
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super(Player, self).__init__()
         self.image = pygame.image.load('jet.png').convert()
         self.image.set_colorkey((255, 255, 255), RLEACCEL)
-        self.rect = self.image.get_rect(bottom = GROUND, left = 0)
+        self.rect = self.image.get_rect(bottom = SKY, left = 0)
         self.velocity = 0
     
     def update(self):
         self.rect.move_ip(0, self.velocity * -1)
-        if self.rect.bottom <= GROUND - JUMP_APEX:
+        if self.rect.bottom <= SKY - JUMP_APEX:
             self.velocity = -1
-        elif self.rect.bottom >= GROUND:
+        elif self.rect.bottom >= SKY:
             self.velocity = 0
 
 
@@ -37,8 +38,8 @@ class Enemy(pygame.sprite.Sprite):
         super(Enemy, self).__init__()
         self.image = pygame.image.load('missile.png').convert()
         self.image.set_colorkey((255, 255, 255), RLEACCEL)
-        self.rect = self.image.get_rect(bottom = GROUND, left = WIDTH)
-        self.speed = 1
+        self.rect = self.image.get_rect(bottom = SKY, left = WIDTH)
+        self.speed = 2
 
     def update(self):
         self.rect.move_ip(-self.speed, 0)
@@ -75,8 +76,11 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 # create our 'player', right now he's just a rectangle
 player = Player()
 
-background = pygame.Surface(screen.get_size())
-background.fill((135, 206, 250))
+background_sky = pygame.Surface((WIDTH, SKY))
+background_sky.fill((0,0,255))
+
+background_ground = pygame.Surface((WIDTH, HEIGHT - SKY))
+background_ground.fill((100,0,100))
 
 enemies = pygame.sprite.Group()
 #clouds = pygame.sprite.Group()
@@ -85,13 +89,13 @@ all_sprites.add(player)
 
 running = True
 time = 0
-
+def playGame():
 while running:
     for event in pygame.event.get():
         if event.type == KEYDOWN:
             if event.key == K_ESCAPE:
                 running = False
-            elif event.key == K_SPACE and player.rect.bottom == GROUND:
+            elif event.key == K_SPACE and player.rect.bottom == SKY:
                 player.velocity = 1
         elif event.type == QUIT:
             running = False
@@ -104,12 +108,13 @@ while running:
             # all_sprites.add(new_cloud)
             # clouds.add(new_cloud)
     time += 1
-    if not time % (CLOCK_TICK * ENEMY_INTERVAL):
+    if not time % (CLOCK_TICK * ENEMY_INTERVAL * ENEMY_WIDTH):
         new_enemy = Enemy()
         enemies.add(new_enemy)
         all_sprites.add(new_enemy)
     if not time % CLOCK_TICK:
-        screen.blit(background, (0, 0))
+        screen.blit(background_sky, (0, 0))
+        screen.blit(background_ground, (0, SKY))
 #    pressed_keys = pygame.key.get_pressed()
         player.update()
         enemies.update()
